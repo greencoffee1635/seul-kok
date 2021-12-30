b62e2927ae51dcbd66414c6074bccf77bf907182
leaisrevolution <leaisrevolution@gmail.com>
2021-12-30 18:16:54 +0900

    commit

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import Image from '/image/image1';
 
 // components
 import Header from '../components/Header';
+// import useMotion from '../utils/useMotion';
 // import Layout from '../components/Layout';
 
 
@@ -13,16 +15,31 @@ const MainPage = (props) => {
   return (
     <SectionWrapper>
       <Header page="main" />
-      <img src={process.env.PUBLIC_URL + `/image/image1.jpg`} alt="" />
+    <SectionImage>
+
+    </SectionImage>
+      {/* <img src={process.env.PUBLIC_URL + `/image/image1.jpg`} alt="" /> */}
     </SectionWrapper>
   );
 };
 
 
-const SectionWrapper = styled.div`
+
+
+const SectionWrapper = styled.article`
   background: var(--black);
   width: 100%;
   height: 100vh;
   `;
+
+const SectionImage = styled.img`
+  width: 100%
+`
+Image.defaultProps = {
+  src: Image,
+};
+
+
+
 
 export default MainPage;
