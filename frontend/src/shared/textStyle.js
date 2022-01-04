5ad4609e3fdfc8c49af12c427a8675355fa9cc32
greencoffee1635 <166354@naver.com>
2022-01-04 17:22:48 +0900

    style: text 스타일 수정

--- frontend/src/shared/textStyle.js
+++ frontend/src/shared/textStyle.js
@@ -2,14 +2,14 @@ import { css } from 'styled-components';
 
 // 로고
 export const logo = css`
-  font-size: 2.3rem;
+  font-size: 1.8rem;
   font-weight: 700;
 `;
 
 // Header Menu
 export const head_1 = css`
-  font-size: 1.3rem;
-  font-weight: 500;
+  font-size: 1rem;
+  font-weight: 400;
 `;
 
 // QuestionPage Title
