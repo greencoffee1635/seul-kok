97b227feb9f525dc2bb8377385b10e96b3fb8f0f
greencoffee1635 <166354@naver.com>
2022-01-08 01:23:24 +0900

    feat: 결과페이지 더미 생성
    Former-commit-id: 08b54dcbd2fbf0910c18dc0b5b4daa6355176c25

---
+++ frontend/src/Data/resultDummy.js
@@ -0,0 +1,62 @@
+export const MovieContentdData = {
+  ott: 'netflix',
+  userName: '오슬콕',
+  contents: [
+    {
+      title: '지금 우리 학교는',
+      genre: '드라마',
+      desc: '좀비가 창궐한 초등학교에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 니네 학교는',
+      genre: '호러',
+      desc: '좀비가 창궐한 중학교에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 걔네 학교는',
+      genre: '코미디',
+      desc: '좀비가 창궐한 고등학교에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 내 학교는',
+      genre: '메디컬',
+      desc: '좀비가 창궐한 대학교에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 애 학교는',
+      genre: '액션',
+      desc: '좀비가 창궐한 유치원에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 그 학교는',
+      genre: '스릴러',
+      desc: '좀비가 창궐한 대학원에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 남의 학교는',
+      genre: '로맨스',
+      desc: '좀비가 창궐한 평생교육원에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+    {
+      title: '지금 그의 학교는',
+      genre: '애니메이션',
+      desc: '좀비가 창궐한 정부에 갇힌 학생들. 필사적으로 탈출하려는 과정에서 극한 상황에 처하게 된다.',
+      poster:
+        'https://nujhrcqkiwag1408085.cdn.ntruss.com/static/upload/drama_poster_images/280x400/drama_88716_1637200184.jfif',
+    },
+  ],
+};
