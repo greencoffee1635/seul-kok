48c00f366948b08c402f7e95f511e734d7fbcce5
[3기-1반] 김산하 <shk8532@gmail.com>
2022-01-08 14:56:28 +0000

    Update __init__.py
    Former-commit-id: 03f718465d6c9fdf6a7ae330f3d86dc091fc9cdc

--- backend/services/web/project/__init__.py
+++ backend/services/web/project/__init__.py
@@ -9,16 +9,7 @@ from flask import (
     redirect,
     url_for
 )
-from flask_jwt_extended import (
-    JWTManager, create_access_token, 
-    get_jwt_identity, jwt_required,
-    set_access_cookies, set_refresh_cookies, 
-    unset_jwt_cookies, create_refresh_token,
-    jwt_refresh_token_required,
-)
-from config import CLIENT_ID, REDIRECT_URI
-from controller import Oauth
-
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import sqlite3
@@ -84,35 +75,6 @@ def upload_file():
       <p><input type=file name=file><input type=submit value=Upload>
     </form>
     """
-
-
-@app.route("/oauth")
-def oauth_api():
-    """
-    # OAuth API [GET]
-    사용자로부터 authorization code를 인자로 받은 후,
-    아래의 과정 수행함
-    1. 전달받은 authorization code를 통해서
-        access_token, refresh_token을 발급.
-    2. access_token을 이용해서, Kakao에서 사용자 식별 정보 획득
-    3. 해당 식별 정보를 서비스 DB에 저장 (회원가입)
-    3-1. 만약 이미 있을 경우, (3) 과정 스킵
-    4. 사용자 식별 id를 바탕으로 서비스 전용 access_token 생성
-    """
-    code = str(request.args.get('code'))
-    
-    oauth = Oauth()
-    auth_info = oauth.auth(code)
-    user = oauth.userinfo("Bearer " + auth_info['access_token'])
-    
-    user = 'seulkok@kakao.com' #useremail
-
-    access_token = create_access_token(identity=user)
-    refresh_token = create_refresh_token(identity=user)
-    
-    data = {'logined':'true', 'access_token':access_token, 'refresh_token':refresh_token}
-
-    return jsonify(data)
 
 
 @app.route("/surveyopen", methods=["GET", "POST"])
